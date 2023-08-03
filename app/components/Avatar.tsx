75e5684c998c8115c277757557d4781c06cc8211
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-03 14:48:35 +0530

    GitHub

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,10 +2,13 @@
 
 import Image from 'next/image';
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
-      src="/images/placeholder.jpg"
+      src={src || '/images/placeholder.jpg'}
       height="30"
       width="30"
       alt="Avatar"
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -61,7 +61,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
