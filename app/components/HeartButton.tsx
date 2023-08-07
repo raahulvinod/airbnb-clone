a1a3097e94d88492220e222e5e25f3b82273746a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-07 17:30:21 +0530

    Favoriting functionality

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,36 +1,48 @@
 'use client';
 
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
-import { SafeUser } from '../types';
 
-interface HeartButonProps {
+import useFavorite from '@/app/hooks/useFavorite';
+import { SafeUser } from '@/app/types';
+
+import ClientOnly from './ClientOnly';
+
+interface HeartButtonProps {
   listingId: string;
   currentUser?: SafeUser | null;
 }
 
-const HeartButton: React.FC<HeartButonProps> = ({ listingId, currentUser }) => {
-  const hasFavourite = false;
-  const toggleFavourite = () => {};
+const HeartButton: React.FC<HeartButtonProps> = ({
+  listingId,
+  currentUser,
+}) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
+
   return (
     <div
-      onClick={toggleFavourite}
+      onClick={toggleFavorite}
       className="
-  relative
-  hover: opacity-80
-  transition
-  cursor-pointer"
+        relative
+        hover:opacity-80
+        transition
+        cursor-pointer
+      "
     >
       <AiOutlineHeart
         size={28}
         className="
-        fill-white 
-        absolute 
-        -top-[2px] 
-        -right-[2px]"
+          fill-white
+          absolute
+          -top-[2px]
+          -right-[2px]
+        "
       />
       <AiFillHeart
         size={24}
-        className={hasFavourite ? 'fill-rose-500' : 'fill-neutral-500/70'}
+        className={hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'}
       />
     </div>
   );
