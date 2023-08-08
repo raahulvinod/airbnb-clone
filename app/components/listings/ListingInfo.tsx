3048a56644b27a843dff4998cf9526cfa3bef89d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-08 12:11:00 +0530

    ListingInfo and ListingCategory  Added

---
+++ app/components/listings/ListingInfo.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import useCountries from '@/app/hooks/useCountries';
+import { SafeUser } from '@/app/types';
+import { IconType } from 'react-icons';
+
+import Avatar from '../Avatar';
+import ListingCategory from './ListingCategory';
+import Map from '../Map';
+
+interface ListingInfoProps {
+  user: SafeUser;
+  description: string;
+  roomCount: number;
+  guestCount: number;
+  bathroomCount: number;
+  locationValue: string;
+  category:
+    | {
+        icon: IconType;
+        label: string;
+        description: string;
+      }
+    | undefined;
+}
+
+const ListingInfo: React.FC<ListingInfoProps> = ({
+  user,
+  category,
+  description,
+  roomCount,
+  guestCount,
+  bathroomCount,
+  locationValue,
+}) => {
+  const { getByValue } = useCountries();
+
+  const coordinates = getByValue(locationValue)?.latlng;
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div
+          className="
+        text-xl
+        font-semibold,
+        flex
+        flex-row
+        items-center
+        gap-2
+        "
+        >
+          <div>Hosted by {user.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div
+          className="
+            flex 
+            flex-row 
+            items-center 
+            gap-4 
+            font-light
+            text-neutral-500
+          "
+        >
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category?.label}
+          description={category?.description}
+        />
+      )}
+      <hr />
+      <div
+        className="
+      text-lg font-light text-neutral-500"
+      >
+        {description}
+      </div>
+      <hr />
+      <Map center={coordinates} />
+    </div>
+  );
+};
+
+export default ListingInfo;
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,6 +2,7 @@
 
 import Container from '@/app/components/Container';
 import ListingHead from '@/app/components/listings/ListingHead';
+import ListingInfo from '@/app/components/listings/ListingInfo';
 import { categories } from '@/app/components/navbar/Categories';
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
@@ -34,6 +35,17 @@ const ListingClient: React.FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
