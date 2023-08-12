85b76f272455fe4f9cb9ada253878b894ad3c92e
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-12 16:37:35 +0530

    Working with Properties Screen

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,8 +1,21 @@
 import prisma from '@/app/libs/prismadb';
 
-export default async function getListings() {
+export interface IListingParams {
+  userId?: string;
+}
+
+export default async function getListings(params: IListingParams) {
   try {
+    const { userId } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: 'desc',
       },
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   onClick={() => router.push('/reservations')}
                   label="My reservations"
                 />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem
+                  onClick={() => router.push('/properties')}
+                  label="My properties"
+                />
                 <MenuItem onClick={rentModel.onOpen} label="Airbnb my home" />
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Logout" />
